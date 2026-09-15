bce4089f73cdeae9157d16f460df58114ccfd46e
Andrey Kozin <andkozin@gmail.com>
2026-09-15 16:31:05 +0300

    Дописал фикстуры

---
+++ conftest.py
@@ -0,0 +1,46 @@
+# conftest.py
+
+import pytest
+from helpers.user_helper import UserHelper
+from helpers.login_helper import LoginHelper
+from helpers.order_helper import OrderHelper
+from helpers.data_generator import get_user_payload
+
+
+@pytest.fixture
+def user_helper():
+    return UserHelper()
+
+
+@pytest.fixture
+def login_helper():
+    return LoginHelper()
+
+@pytest.fixture
+def registered_user(user_helper):
+    payload = get_user_payload()
+
+    access_token = user_helper.create_and_get_token(
+        payload["email"],
+        payload["password"],
+        payload["name"]
+    )
+
+    yield {
+        "email": payload["email"],
+        "password": payload["password"],
+        "name": payload["name"],
+        "access_token": access_token,
+    }
+
+    user_helper.delete_user(access_token)
+    
+
+@pytest.fixture
+def order_helper():
+    return OrderHelper()
+
+@pytest.fixture
+def ingredient_hashes(order_helper):
+    return order_helper.get_bun_and_main_ids()
+
